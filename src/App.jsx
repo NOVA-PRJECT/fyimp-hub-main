9851b8472f995fc19c3489cd9b8e39fc4ae5e7f7
zayyan <muhdzay786@gmail.com>
2025-12-17 15:27:29 +0530

    completed navbar,work pending on input state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import NavBar from './components/NavBar';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  return (
-  )
+  const [sidebar ,setsidebar] = useState(false);
+  const [darkMode ,setdarkMode] = useState(false);
+  
+  const toggleDarkMode = () => {
+    setdarkMode(!darkMode);
+  }
 
+  const toggleSidebar = () => {
+    setsidebar(!sidebar);
+  }
+  return (
+    <div>
+      <NavBar toggleSidebar={toggleSidebar} darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <h1>THE WEBSITE IS UNDER CONSTRUCTION</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import { Menu, Brain, Search, Moon, Sun } from "lucide-react"
+import React, { useState } from "react";
+import "../styles/NavBar.css"
+
+
+function NavBar({ toggleSidebar, darkMode, toggleDarkMode }) {
+    const [searching, setsearching] = useState(false);
+
+    const handleSearchClick = () => {
+        setsearching(!searching);
+    }
+    return (
+        <header className="header">
+            <header className="header">
+                {!searching ? (
+                    <div className="searchInput">
+                        <input type="text" className="inputBar" placeholder="Search from all papers..." />
+                        <button onClick={handleSearchClick} className="closeSearchBtn">
+                            X
+                        </button>
+                    </div>
+                ) : (
+                    <div className="navbar">
+                        <div className="leftNav">
+                            <button onClick={toggleSidebar} className="menuBtn">
+                                <Menu className="hamburgerIcon" />
+                            </button>
+                            <span className="logoText">
+                                FYIMP H<Brain className="logoBrain" />B
+                            </span>
+                        </div>
+                        <div className="rightNav">
+                            <Search onClick={handleSearchClick} className="searchIcon" />
+                            {darkMode ? <Sun onClick={toggleDarkMode} className="darkModeIcon" /> : <Moon onClick={toggleDarkMode} className="darkModeIcon" />
+                            }
+                        </div>
+                    </div>
+                )}
+            </header>
+
+        </header>
+
+    )
+
+}
+export default NavBar;
