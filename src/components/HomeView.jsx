87016cb3de3e0ddd3af64360b36c774e44aaa226
Zayyan <muhdzay786@gmail.com>
2025-12-21 11:20:07 +0530

    homeview staryed

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -6,9 +6,13 @@ function HomeView (){
   
   return (
     <div className="home">
-  
+     <div className="quotediv">
+       <p className="quotetext">Knowledge grows strongest when it is shared, structured, and preserved.</p>
+     </div>
+     <p class="texthamhome">
+  Click <span class="ham">&#9776;</span> on the top left to select your department
+    </p>
     </div>
-    
     )
 }
 
