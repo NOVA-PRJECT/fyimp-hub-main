1e7affad39a13892c73e84aa11059de3d4a4bb38
Faaz P V <faazpv09@gmail.com>
2025-12-23 17:56:43 +0530

    worked on supabase papers done

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,35 @@ import BottomNavBar from './components/BottomNavBar'
 import HomeView from './components/HomeView'
 import PaperView from './components/PaperView'
 function App() {
-  const [darkMode ,setdarkMode] = useState(false);
-      const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-      const [departments,setdepartments] =useState([])
-      const[selectedDept, setselectedDept] = useState(null);
-      const [selectedSem,setselectedSem] =useState(1);
+  const [darkMode, setdarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [departments, setdepartments] = useState([])
+  const [selectedDept, setselectedDept] = useState(null);
+  const [selectedSem, setselectedSem] = useState(1);
+  const [papers,setpapers] = useState([]);
 
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
-  
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   const toggleDarkMode = () => {
     setdarkMode(!darkMode);
   }
-  
-  
+
+
   return (
     <div>
-      <NavBar  darkMode={darkMode} toggleDarkMode={toggleDarkMode} toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen}
-      setisSidebarOpen={setIsSidebarOpen}
-      setselectedDept={setselectedDept}/>
+      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen}
+        setisSidebarOpen={setIsSidebarOpen}
+        setselectedDept={setselectedDept} />
       <SideBar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} departments={departments} setdepartments={setdepartments}
-      setselectedDept={setselectedDept}/>
-          {selectedDept === null ? (
+        setselectedDept={setselectedDept} />
+      {selectedDept === null ? (
         <HomeView />
       ) : (
-        <PaperView selectedDept={selectedDept} selectedSem={selectedSem }/>
-        )}
-      <BottomNavBar setselectedSem={setselectedSem}/>
+        <PaperView selectedDept={selectedDept} selectedSem={selectedSem} setpapers={setpapers} papers={papers}/>
+      )}
+      <BottomNavBar setselectedSem={setselectedSem} />
     </div>
   );
 }
--- src/components/PaperView.jsx
+++ src/components/PaperView.jsx
@@ -1,14 +1,42 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import "../styles/PaperView.css"
+import { supabase } from "../supabaseClient";
+
+function PaperView({selectedDept,selectedSem,setpapers,papers}){
+
+  const paperOrder=["DSC","DSE","MDC","SEC","VAC"];
+  
+async function fetchpapers() {
+   const {data,error} = await supabase
+.from("papers")
+.select("*")
+.eq("department_id",selectedDept)
+.eq("semester",selectedSem)
+.eq("is_active",true);
+ if (error){
+   console.log("error : ",error);
+ }
+ setpapers(data || []);
+}
+
+useEffect(()=>{
+  fetchpapers();
+},[selectedDept,selectedSem])
 
 
-function PaperView({selectedDept,selectedSem}){
-  
-  
+const orderedPaper=[...papers].sort((a,b)=>{return paperOrder.indexOf(a.type)-paperOrder.indexOf(b.type);});
+
   return(
     <div className="paperview">
       <h4>{selectedDept.charAt(0).toUpperCase() +
-    selectedDept.slice(1).toLowerCase()}&nbsp;>>&nbsp;{selectedSem} Sem</h4>
+    selectedDept.slice(1).toLowerCase()}&nbsp; &nbsp;{selectedSem} Sem</h4>
+      <ul className="paperList">
+  {orderedPaper.map((papers,index) => (
+    <li  className="paperItem" key={papers.id}>
+      {papers.name} ----{index+1}  -----{papers.type}
+    </li>
+  ))}
+</ul>
     </div>
     
     
